e3a1afed1d0a8f8b18d0812e05efbe38d5deb27e
JaumeMontagut <36292291+JaumeMontagut@users.noreply.github.com>
2018-10-07 15:11:00 +0200

    Player can has access to the SDL_SCANCODES
    That fixed an error where they were undefined.

--- Motor2D/j1Player.cpp
+++ Motor2D/j1Player.cpp
@@ -1,6 +1,8 @@
 #include "j1Player.h"
 #include "j1Input.h"
 #include "j1App.h"
+#include "j1Render.h"
+
 
 bool j1Player::Awake(pugi::xml_node&)
 {
@@ -19,9 +21,18 @@ bool j1Player::PreUpdate()
 
 bool j1Player::Update(float dt)
 {
-	if (App->input->GetKey(SDL_SCANCODE_LEFT) == KEY_DOWN) {
+	if (App->input->GetKey(SDL_SCANCODE_UP) == KEY_REPEAT)
+		App->render->camera.y += 1;
 
-	}
+	if (App->input->GetKey(SDL_SCANCODE_DOWN) == KEY_REPEAT)
+		App->render->camera.y -= 1;
+
+	if (App->input->GetKey(SDL_SCANCODE_LEFT) == KEY_REPEAT)
+		App->render->camera.x += 1;
+
+	if (App->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT)
+		App->render->camera.x -= 1;
+
 	return true;
 }
 
